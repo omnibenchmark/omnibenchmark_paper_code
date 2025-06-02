b54754057fa7a7a348a7d0b5c8a91faff40c2893
Izaskun Mallona <izaskun.mallona@gmail.com>
2025-06-02 11:47:10 +0200

    Update singularity recipe (unpinned version)

---
+++ Makefile
@@ -0,0 +1,16 @@
+MAX_CORES ?= 12
+
+# with continue on error (-k)
+OB_CMD=ob run benchmark -k --local --cores ${MAX_CORES} --yes
+
+# prepare_apptainer_env:
+# 	cd envs && bash build_singularity.sh
+
+run_with_apptainer_unpinned:
+	 ${OB_CMD} -b Clustering_singularity.yml
+	 mv out out_singularity_$(shell date +'%Y%m%d%H%M')
+run_with_conda:
+	 ${OB_CMD} -b Clustering_conda.yml
+	 mv out out_conda_$(shell date +'%Y%m%d%H%M')
+
+## knit_marks_report:
--- envs/build_singularity.sh
+++ envs/build_singularity.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-sudo singularity build sklearn.sif sklearn_singularity.def
+singularity build sklearn.sif sklearn_singularity.def
 
-sudo singularity build clustbench.sif clustbench_singularity.def
+singularity build clustbench.sif clustbench_singularity.def
 
-sudo singularity build r.sif r_singularity.def
+singularity build r.sif r_singularity.def
 
-sudo singularity build fcps.sif fcps_singularity.def
+singularity build fcps.sif fcps_singularity.def
