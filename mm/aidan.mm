c13356266293745f93efee8e30dfd3b4eff2a2ef
Mark Robinson <mark.robinson@mls.uzh.ch>
2026-08-10 15:34:49 -0400

    update to increase font size

--- mm/aidan.mm
+++ mm/aidan.mm
@@ -10,37 +10,29 @@ flowchart TB
   end
  subgraph methods["methods"]
         boostgp["boostgp"]
-        nnsvg["nnsvg"]
+        gpcounts["gpcounts"]
         spark["spark"]
         spark_x["spark_x"]
-        gpcounts["gpcounts"]
         moran_i["moran_i"]
+        nnsvg["nnsvg"]
         scgco["scgco"]
-        sepal["sepal"]
-        somde["somde"]
-        spagcn["spagcn"]
-        spagft["spagft"]
-        spanve["spanve"]
-        spatialde["spatialde"]
+        SpaGCN["SpaGCN"]
         spatialde2["spatialde2"]
+        ...["..."]
   end
  subgraph metric["metric"]
         correlation["correlation"]
   end
-    load_spatial_data --> boostgp & nnsvg & spark & spark_x & gpcounts & moran_i & scgco & sepal & somde & spagcn & spagft & spanve & spatialde & spatialde2
+    load_spatial_data --> boostgp & gpcounts & spark & spark_x & moran_i & nnsvg & scgco & SpaGCN & spatialde2 & ...
     boostgp --> correlation
     gpcounts --> correlation
+    spark --> correlation
+    spark_x --> correlation
     moran_i --> correlation
     nnsvg --> correlation
     scgco --> correlation
-    sepal --> correlation
-    somde --> correlation
-    spagcn --> correlation
-    spagft --> correlation
-    spanve --> correlation
-    spark --> correlation
-    spark_x --> correlation
-    spatialde --> correlation
+    SpaGCN --> correlation
     spatialde2 --> correlation
-    
+    ... --> correlation
+
     classDef param fill:#f96
